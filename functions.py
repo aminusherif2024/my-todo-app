231461651a093ab1a18929c9720b8314be120bec
aminusherif2024 <aminu.sherif45@gmail.com>
2025-01-10 15:58:33 +0530

    Initial commit

---
+++ functions.py
@@ -0,0 +1,23 @@
+FILEPATH = r"C:\Users\aminu\PycharmProjects\PythonProject1\experiments\todos.txt"
+
+
+def get_todos(filepath=FILEPATH):
+    '''Read a text file and return the list
+    of to-do items'''
+    with open(filepath, "r") as file_local:
+        todos_local = file_local.readlines()
+    return todos_local
+
+
+def write_todos(todos_arg, filepath=FILEPATH ):
+    ''' Write the to-do items list in the text file'''
+
+    with open(filepath, "w") as file:
+        file.writelines(todos_arg)
+
+
+
+
+
+if __name__=="__main__":
+    print("Hello")
